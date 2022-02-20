f4c378241767fcb743b41ccfb1b1b7adb2dab7e9
Braeae <91569703+Braeae@users.noreply.github.com>
2022-02-20 13:36:52 +0100

    Files created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Learn React</p>
+        <button className="btn btn-primary">Click here</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
