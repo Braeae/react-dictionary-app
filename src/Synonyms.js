8d176bae5da337689e77547ce42e8a28aeab3c7a
Braeae <91569703+Braeae@users.noreply.github.com>
2022-02-26 20:30:49 +0100

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,8 +4,9 @@ function Synonyms(props) {
   if (props.synonyms) {
     return (
       <div className="Synonyms">
+        <strong>Synonyms: </strong>
         {props.synonyms.map(function (synonym, index) {
-          return <span key={index}>{synonym}</span>;
+          return <span key={index}>{synonym} </span>;
         })}
       </div>
     );
